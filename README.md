9b31094be7431c706beccc4eeee50dac438d70b2
Diamond Lewis <findlewis@gmail.com>
2019-06-19 12:22:14 -0500

    Add README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sign in with Apple Test
+
+https://developer.okta.com/blog/2019/06/04/what-the-heck-is-sign-in-with-apple
+
+https://developer.apple.com/sign-in-with-apple/get-started/
---
+++ client_secret.rb
@@ -0,0 +1,21 @@
+    require 'jwt'
+
+# Save your private key from Apple in a file called `key.txt`
+
+ecdsa_key = OpenSSL::PKey::EC.new IO.read 'key.txt'
+
+headers = {
+  'kid' => ''                           # Find the 10-char Key ID value from the portal
+}
+
+claims = {
+  'iss' => '',                          # Your 10-character Team ID
+  'iat' => Time.now.to_i,
+  'exp' => Time.now.to_i + 86400*30,    # This will be valid for 30 days
+  'aud' => 'https://appleid.apple.com', 
+  'sub' => '',                          # Your Services ID, e.g. com.aaronparecki.services
+}
+
+token = JWT.encode claims, ecdsa_key, 'ES256', headers
+
+puts token
